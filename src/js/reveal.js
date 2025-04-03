e28c531c0de5fee82ddaaeeb47912335ecf24c93
Weener Brasil Estoque (1661) Unidade GRSA <unop1661e@grsa.com.br>
2025-04-03 16:42:07 -0300

    Atualização de fonte do titulo, adição do favicon e adicionado efeitos utilizando scrollreveal

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -1,168 +1,32 @@
-// window.revelar = ScrollReveal({ reset:true });
+window.revelar = ScrollReveal({ reset:true });
 
-// //FLECHA
-// revelar.reveal('.bi-arrow-down', {
-//     duration: 2000,
-//     distance: '100px',
-//     origin: 'bottom'
-// });
+revelar.reveal('.animation-top', {
+    duration: 1500,
+    distance: '60px',
+    origin: 'top'
+});
 
-// revelar.reveal('.suzane-sales', {
-//     duration: 2000,
-//     distance: '1000px',
-//     origin: 'bottom'
-// });
+revelar.reveal('.animation-bottom', {
+    duration: 1500,
+    distance: '100px',
+    origin: 'bottom'
+});
 
-// //SOBRE
-// revelar.reveal('.animation-left', {
-//     duration: 1000,
-//     distance: '1000px',
-//     delay: 150,
-//     origin: 'left'
-// });
+revelar.reveal('.animation-left', {
+    duration: 1500,
+    distance: '100px',
+    origin: 'left'
+});
 
-// revelar.reveal('.sobre-title-animation', {
-//     duration: 2000,
-//     distance: '100px',
-//     origin: 'top'
-// });
+revelar.reveal('.animation-right', {
+    duration: 1500,
+    distance: '100px',
+    origin: 'right'
+});
 
-// revelar.reveal('.sobre-text-animation', {
-//     duration: 2000,
-//     distance: '100px',
-//     delay: 150
-// });
-
-
-// revelar.reveal('.animation-right', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 150,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-html5', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 100,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-css3', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 150,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-js', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 200,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-react', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 250,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-database', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 300,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-node', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 350,
-//     origin: 'rigth'
-// });
-
-// revelar.reveal('.fa-github', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 400,
-//     origin: 'rigth'
-// });
-
-
-// //PROJETOS ANIMATION
-// revelar.reveal('.fundo1', {
-//     duration: 2000,
-//     distance: '1000px',
-//     origin: 'left'
-// });
-
-// revelar.reveal('.fundo2', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 150,
-//     origin: 'left'
-// });
-
-// revelar.reveal('.fundo3', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 200,
-//     origin: 'left'
-// });
-
-// revelar.reveal('.projetos-title-animation', {
-//     duration: 2000,
-//     distance: '500px',
-//     origin: 'left'
-// });
-
-// revelar.reveal('.projetos-text-animation', {
-//     duration: 2000,
-//     distance: '1000px',
-//     delay: 150,
-//     origin: 'left'
-// });
-
-// revelar.reveal('.projetos-list-animation', {
-//     duration: 2000,
-//     distance: '100px',
-//     delay: 200,
-//     origin: 'bottom'
-// });
-
-// revelar.reveal('.image-animation1', {
-//     duration: 2000,
-//     distance: '500px',
-//     origin: 'bottom'
-// });
-
-// revelar.reveal('.image-animation2', {
-//     duration: 2000,
-//     distance: '500px',
-//     delay: 250,
-//     origin: 'bottom'
-// });
-
-// revelar.reveal('.image-animation3', {
-//     duration: 2000,
-//     distance: '500px',
-//     delay: 500,
-//     origin: 'bottom'
-// });
-
-// //CONTATO
-// revelar.reveal('.contato-img', {
-//     duration: 2000,
-//     distance: '100px',
-//     delay: 250,
-//     origin: 'bottom'
-// });
-
-// revelar.reveal('.title', {
-//     duration: 2000,
-//     distance: '100px',
-//     delay: 150,
-//     origin: 'top'
-// });
+revelar.reveal('.animation-projetos', {
+    duration: 2000,
+    interval: 10,
+    distance: '1000px',
+    origin: 'bottom'
+});
