22e3e052f838cd1b495d1054e52b334692d8ad1f
Weener Brasil Estoque (1661) Unidade GRSA <unop1661e@grsa.com.br>
2025-04-04 12:27:59 -0300

    atualização no js script e reveal, pois em alguns casos o reveal não funcionava corretamente na sessão de projetos.

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -1,32 +1,9 @@
 window.revelar = ScrollReveal({ reset:true });
 
-revelar.reveal('.animation-top', {
-    duration: 1500,
-    distance: '60px',
-    origin: 'top'
-});
-
-revelar.reveal('.animation-bottom', {
-    duration: 1500,
-    distance: '100px',
-    origin: 'bottom'
-});
-
-revelar.reveal('.animation-left', {
-    duration: 1500,
-    distance: '100px',
-    origin: 'left'
-});
-
-revelar.reveal('.animation-right', {
-    duration: 1500,
-    distance: '100px',
-    origin: 'right'
-});
-
-revelar.reveal('.animation-projetos', {
-    duration: 2000,
-    interval: 10000,
-    distance: '1000px',
-    origin: 'bottom'
-});
+function aplicarAnimacao(classe, duracao, distancia, origem) {
+    revelar.reveal(classe, {
+        duration: duracao,
+        distance: distancia,
+        origin: origem,
+    });
+}
--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const apresentacao = document.createElement("div")
             apresentacao.className = "row mb-5 position-relative"
             apresentacao.innerHTML = `
-                <div class="col-12 col-md-6 mb-5">
+                <div class="animation-left col-12 col-md-6 mb-5">
                     <p class="fundo top-100px">Projeto</p>
                     <p class="fundo top-300px start-50px">${projeto.title}</p>
                     <h2 class="title display-2">${projeto.title}</h2>
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     )).join('')}       
                     </ul>
                 </div>
-                <div class="col-12 col-md-6 d-flex mb-5">
+                <div class="animation-right col-12 col-md-6 d-flex mb-5">
                     ${projeto.images.map((img) => (
                         `
                             <div class="d-flex align-items-center px-2">
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     )).join('')}
                 </div>
 
-                <div class="rol-12 w-auto mx-auto d-flex flex-column flex-md-row gap-md-2 gap-3 mt-5">
+                <div class="animation-bottom rol-12 w-auto mx-auto d-flex flex-column flex-md-row gap-md-2 gap-3 mt-5">
                     <a class="link btn custom-btn" href="${projeto.repository}" target="_blank">
                         Ir para o repositório
                         <i class="bi bi-github"></i>
@@ -47,6 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 </div>
             `
             section.appendChild(apresentacao)
+            
+            aplicarAnimacao('.animation-top', 1500, '60px', 'top');
+            aplicarAnimacao('.animation-left', 1500, '100px', 'left');
+            aplicarAnimacao('.animation-right', 1500, '100px', 'right');
+            aplicarAnimacao('.animation-bottom', 1500, '100px', 'bottom');
         })
     })
     .catch(error => console.error('Erro ao carregar os dados:', error));
